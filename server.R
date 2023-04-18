e8f917782f0cba88dd4576df243cf31435f3b0f1
emilylundberg <57082438+emilylundberg@users.noreply.github.com>
2023-04-18 13:37:43 -0600

    Add files via upload
    This version now has three tabs - one for the cepcorr factors, one for cepcorr vs sii, and one for cepcorr vs rmse. Still need to fix the theme, filter out the target data, and add a fourth tab to look at test-target differences.

--- server.R
+++ server.R
@@ -32,42 +32,79 @@ csshiny_df$Quiet_Noise <- factor(csshiny_df$Quiet_Noise, levels =
 
 
 # Define server logic required to draw a histogram
-function(input, output, session) {
+server <- function(input, output, session) {
 
-    plot_data <- reactive({
+    plot_data_fct <- reactive({
       df <- csshiny_df %>%
-        filter(Notch == input$Notch) %>%
-        filter(WB_LP == input$WB_LP) %>%
-        filter(RefMatch == input$RefMatch)
+        filter(Notch == input$notch_fct) %>%
+        filter(WB_LP == input$wb_lp_fct) %>%
+        filter(RefMatch == input$ref_fct) 
       
-      if (input$Lin_nonlin %in% c("Linear", "Nonlinear")) {
-        df <- df %>% filter(Lin_nonlin == input$Lin_nonlin)
+      if (input$lin_nonlin_fct %in% c("Linear", "Nonlinear")) {
+        df <- df %>% filter(Lin_nonlin == input$lin_nonlin_fct)
       }
       
-      if (input$Quiet_Noise == "Quiet") {
-        df <- df %>% filter(Quiet_Noise == input$Quiet_Noise)
+      if (input$qn_fct == "Quiet") {
+        df <- df %>% filter(Quiet_Noise == input$qn_fct)
       }
       
-      if (input$ProcMinMax == "Min and Max Processing Levels") {
+      if (input$min_max_fct == "Min and Max Processing Levels") {
         df <- df %>% filter(ProcMinMax %in% c("Min", "Max"))
       }
       
       return(df)
     })
     
+    plot_data_sii <- reactive({
+      df <- csshiny_df %>%
+        filter(Notch == input$notch_sii) %>%
+        filter(WB_LP == input$wb_lp_sii) %>%
+        filter(RefMatch == input$ref_sii) %>%
+        filter(Level == input$level_sii) %>% 
+        filter(Lin_nonlin == input$lin_nonlin_sii) %>% 
+        filter(Quiet_Noise == 'Quiet') %>%
+        mutate(Processing = as_factor(Processing))
+      
+      if (input$min_max_sii == "Min and Max Processing Levels") {
+        df <- df %>% filter(ProcMinMax %in% c("Min", "Max"))
+      }
+      
+      return(df)
+    })
+    
+    plot_data_rmse <- reactive({
+      df <- csshiny_df %>%
+        filter(Notch == input$notch_rmse) %>%
+        filter(WB_LP == input$wb_lp_rmse) %>%
+        filter(RefMatch == input$ref_rmse) %>%
+        filter(Level == input$level_rmse) %>% 
+        filter(Lin_nonlin == input$lin_nonlin_rmse) %>% 
+        filter(Quiet_Noise == 'Quiet') %>%
+        mutate(Processing = as_factor(Processing))
+      
+      if (input$min_max_rmse == "Min and Max Processing Levels") {
+        df <- df %>% filter(ProcMinMax %in% c("Min", "Max"))
+      }
+      
+      return(df)
+      
+    })
+    
+    
     output$factorPlot <- renderPlotly({
         
-      if (input$Quiet_Noise == "Quiet and Noise") {
-        factor_plot <- ggplot(plot_data(), aes(x = Level, y = HASQICepCorr,
+      if (input$qn_fct == "Quiet and Noise") {
+        factor_plot <- ggplot(plot_data_fct(), aes(x = Level, y = HASQICepCorr,
                                       group = interaction(Processing, Quiet_Noise),
                                       color = Processing, linetype = Quiet_Noise)) 
-      } else if (input$Lin_nonlin == "Both Nonlinear and Linear") {
-        factor_plot <- ggplot(plot_data(), aes(x = Level, y = HASQICepCorr,
+        
+      } else if (input$lin_nonlin_fct == "Both Nonlinear and Linear") {
+        factor_plot <- ggplot(plot_data_fct(), aes(x = Level, y = HASQICepCorr,
                                       group = interaction(Processing, Lin_nonlin),
                                       color = Processing, linetype = Lin_nonlin))
       } else {
         
-        factor_plot <- ggplot(plot_data(), aes(x = Level, y = HASQICepCorr, color = Processing)) 
+        factor_plot <- ggplot(plot_data_fct(), aes(x = Level, y = HASQICepCorr, color = Processing)) 
       }
    
       factor_plot <- factor_plot +
@@ -78,6 +115,7 @@ function(input, output, session) {
         labs(x = "Level (dB)") +
         labs(y = "HASQI CepCorr") +
         scale_y_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80, 1.00), limits = c(0, 1.00)) +
+        scale_x_continuous(breaks = c(55, 65, 75), limits = c(55, 75)) +
         facet_wrap(~ParticipantID) 
       
       factor_plot <- ggplotly(factor_plot)
@@ -85,4 +123,37 @@ function(input, output, session) {
       factor_plot
     })
 
+    output$siiPlot <- renderPlotly({
+      
+      sii_plot <- ggplot(plot_data_sii(), aes(x = sii, y = HASQICepCorr, color = Processing))  +
+        geom_point() +
+        scale_color_carto_d(palette = 'Prism') +
+        theme_minimal() +
+        labs(x = "SII") +
+        labs(y = "HASQI CepCorr") +
+        scale_y_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80, 1.00), limits = c(0, 1.00)) +
+        scale_x_continuous(breaks = c(0, 20, 40, 60, 80, 100), limits = c(0, 100)) +
+        facet_wrap(~ParticipantID) 
+      
+      sii_plot <- ggplotly(sii_plot)
+      
+      sii_plot
+    })
+    
+    output$rmsePlot <- renderPlotly({
+      
+      rmse_plot <- ggplot(plot_data_rmse(), aes(x = rmse, y = HASQICepCorr, color = Processing))  +
+        geom_point() +
+        scale_color_carto_d(palette = 'Prism') +
+        theme_minimal() +
+        labs(x = "RMSE") +
+        labs(y = "HASQI CepCorr") +
+        scale_y_continuous(breaks = c(0, 0.20, 0.40, 0.60, 0.80, 1.00), limits = c(0, 1.00)) +
+       # scale_x_continuous(breaks = c(0, 20, 40, 60, 80, 100), limits = c(0, 30)) +
+        facet_wrap(~ParticipantID) 
+      
+      rmse_plot <- ggplotly(rmse_plot)
+      
+      rmse_plot
+    })
 }
--- ui.R
+++ ui.R
@@ -9,38 +9,83 @@
 
 library(shiny)
 library(shinythemes)
+library(shinydashboard)
 library(plotly)
 
-# Define UI for application that draws a histogram
-fluidPage(theme = shinytheme("flatly"),
+# Define UI for application
+# fluidPage(theme = shinytheme("flatly"))
+          
 
     # Application title
-    titlePanel("HASQI CepCorr Calculation Factors"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("Notch", "Receiver Notch:",
-                        choices = c('NoNotch', 'YesNotch'), selected = 'NoNotch'),
-            selectInput("WB_LP", "Filter Bandwidth:",
-                        choices = c('WB', 'LP'), selected = 'WB'),
-            selectInput("RefMatch", "Reference Level:",
-                        choices = c('65', 'Match'), selected = '65'),
-            radioButtons("Lin_nonlin", "Nonlinear or Linear Fitting?",
-                         choices = c('Nonlinear', 'Linear', 'Both Nonlinear and Linear'), selected = 'Nonlinear'),
-            radioButtons("Quiet_Noise", "Include Quiet and Noise?",
-                        choices = c('Quiet', 'Quiet and Noise'), selected = 'Quiet'),
-            radioButtons("ProcMinMax", "Included Processing Levels:",
-                         choices = c('All Processing Levels', 'Min and Max Processing Levels'), 
-                         selected = 'All Processing Levels')
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          
-            tabsetPanel(type = "tabs",
-                        tabPanel("HASQI CepCorr", plotlyOutput("factorPlot")),
-                        tabPanel("SII Comparison", plotlyOutput("siiPlot"))
-        )
+header <- dashboardHeader(title = "HASQI CepCorr Calculation Factors")
+    
+    #create conditional sidebars
+sidebar <- dashboardSidebar(
+      conditionalPanel(condition = "input.tabselected==1",
+                       selectInput("notch_fct", "Receiver Notch:",
+                                   choices = c('NoNotch', 'YesNotch'), selected = 'NoNotch'),
+                       selectInput("wb_lp_fct", "Filter Bandwidth:",
+                                   choices = c('WB', 'LP'), selected = 'WB'),
+                       selectInput("ref_fct", "Reference Level:",
+                                   choices = c('65', 'Match'), selected = '65'),
+                       radioButtons("lin_nonlin_fct", "Nonlinear or Linear Fitting?",
+                                    choices = c('Nonlinear', 'Linear', 'Both Nonlinear and Linear'), selected = 'Nonlinear'),
+                       radioButtons("qn_fct", "Include Quiet and Noise?",
+                                    choices = c('Quiet', 'Quiet and Noise'), selected = 'Quiet'),
+                       radioButtons("min_max_fct", "Included Processing Levels:",
+                                    choices = c('All Processing Levels', 'Min and Max Processing Levels'), 
+                                    selected = 'All Processing Levels'),
+                       ),
+      conditionalPanel(condition="input.tabselected==2",
+                       selectInput("notch_sii", "Receiver Notch:",
+                                   choices = c('NoNotch', 'YesNotch'), selected = 'NoNotch'),
+                       selectInput("wb_lp_sii", "Filter Bandwidth:",
+                                   choices = c('WB', 'LP'), selected = 'WB'),
+                       selectInput("ref_sii", "Reference Level:",
+                                   choices = c('65', 'Match'), selected = '65'),
+                       radioButtons("lin_nonlin_sii", "Nonlinear or Linear Fitting?",
+                                    choices = c('Nonlinear', 'Linear'), selected = 'Nonlinear'),
+                       radioButtons("min_max_sii", "Included Processing Levels:",
+                                    choices = c('All Processing Levels', 'Min and Max Processing Levels'), 
+                                    selected = 'All Processing Levels'),
+                       radioButtons("level_sii", "Select Input Level:",
+                                    choices = c(55, 65, 75), selected = 65),
+      ),
+      conditionalPanel(condition="input.tabselected==3",
+                       selectInput("notch_rmse", "Receiver Notch:",
+                                   choices = c('NoNotch', 'YesNotch'), selected = 'NoNotch'),
+                       selectInput("wb_lp_rmse", "Filter Bandwidth:",
+                                   choices = c('WB', 'LP'), selected = 'WB'),
+                       selectInput("ref_rmse", "Reference Level:",
+                                   choices = c('65', 'Match'), selected = '65'),
+                       radioButtons("lin_nonlin_rmse", "Nonlinear or Linear Fitting?",
+                                    choices = c('Nonlinear', 'Linear'), selected = 'Nonlinear'),
+                       radioButtons("min_max_rmse", "Included Processing Levels:",
+                                    choices = c('All Processing Levels', 'Min and Max Processing Levels'), 
+                                    selected = 'All Processing Levels'),
+                       radioButtons("level_rmse", "Select Input Level:",
+                                    choices = c(55, 65, 75), selected = 65),
+      )
     )
+
+#Body
+body <- dashboardBody(
+  mainPanel(
+    tabsetPanel(
+      tabPanel("HASQI CepCorr Calculations", value = 1,
+               h4(plotlyOutput("factorPlot"))),
+      tabPanel("CepCorr vs. SII", value = 2,
+               h4(plotlyOutput("siiPlot"))),
+      tabPanel("CepCorr vs. RMSE", value = 3,
+               h4(plotlyOutput("rmsePlot"))),
+      id = "tabselected"
+               
+    )
+  )
 )
+
+ui <- fluidPage(theme = shinytheme("flatly"), 
+                dashboardPage(header, sidebar, body))
+
+
+    
